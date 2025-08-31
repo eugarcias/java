833be59c82db21e2340ed43733e63a8ea12482f0
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-08-31 17:03:26 -0300

    att

--- exercicios/exercicio014/src/exercicio014/Exercicio014.java
+++ exercicios/exercicio014/src/exercicio014/Exercicio014.java
@@ -4,17 +4,31 @@
  */
 package exercicio014;
 
-/**
- *
- * @author Ericaa
- */
+//Solicitar o ano de nascimento de uma pessoa e o ano atual. Verifique
+//se o ano de nascimento é válido, ou seja, se o ano de nascimento é
+//menor que o ano atual, então mostre a idade desta pessoa.
+
+import javax.swing.JOptionPane;
+
 public class Exercicio014 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+
+        int anonasc = Integer.parseInt(JOptionPane.showInputDialog("Qual seu ano de nascimento? "));
+
+        int anoatual = Integer.parseInt(JOptionPane.showInputDialog("Qual o ano atual? "));
+
+        if (anonasc < anoatual) {
+
+            int idade = (anoatual - anonasc);
+            JOptionPane.showMessageDialog(null, "Sua idade é! " + idade + "anos.");
+        } else {
+            JOptionPane.showMessageDialog(null, "O ano de nascimento não pode ser maior ou igual ao ano atual!");
+        }
     }
-    
 }
+
