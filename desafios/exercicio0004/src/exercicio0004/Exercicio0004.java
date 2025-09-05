c0fb3d7ab538ebd2f02ae89541c3c2d32f2d1430
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-09-05 06:51:48 -0300

    att

---
+++ desafios/exercicio0004/src/exercicio0004/Exercicio0004.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio0004;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Exercicio0004 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int idade = 10;
+        System.out.println("Minha idade é: " + idade);
+    }
+    
+}
