f1438ac9956d2079a4ddfee03a875508e01c89bb
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-09-07 18:01:15 -0300

    att

---
+++ exercicios/exercicio0005/src/exercicio0005/Exercicio0005.java
@@ -0,0 +1,15 @@
+
+package exercicio0005;
+
+import javax.JOptionPane; 
+public class Exercicio0005 {
+
+    
+    public static void main(String[] args) {
+        
+        
+       String nome;
+               nome = JOptionPane.showInputDialog("Escreva seu nome");
+    }
+    
+}
