716f3c318bc9887d151577a4c49fb7b2aeddf93e
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-09-21 19:57:06 -0300

    att

--- exercicios/exercicio028/src/Model/Aluno.java
+++ exercicios/exercicio028/src/Model/Aluno.java
@@ -1,4 +1,33 @@
 package Model;
 
 public class Aluno {
+    private String curso;
+    private int fase;
+
+    public Aluno(){
+
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public Aluno (String curso, int fase){
+        this.curso = curso;
+        this.fase = fase;
+
+    }
+
+    public int getFase() {
+        return fase;
+    }
+
+    public void setFase(int fase) {
+        this.fase = fase;
+    }
 }
+
--- exercicios/exercicio028/src/Model/Professor.java
+++ exercicios/exercicio028/src/Model/Professor.java
@@ -1,4 +1,33 @@
 package Model;
 
 public class Professor {
+    private double salario;
+    private String profissao;
+
+
+    public Professor (){
+
+    }
+
+    public Professor (double salario, String profissao){
+        this.salario = salario;
+        this.profissao = profissao;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public String getProfissao() {
+        return profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
 }
+
