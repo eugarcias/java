30ab6a2be9f4f5cde00e6e41985d7b03de9fed03
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-09-22 22:20:30 -0300

    exercicios operadores lógicos

--- maratona-devdojo/exercicios/src/aula1/Aula04Operadores.java
+++ maratona-devdojo/exercicios/src/aula1/Aula04Operadores.java
@@ -22,11 +22,21 @@ public class Aula04Operadores {
         boolean isDezMenorQueVinte = 10 < 20;
         boolean isDezIgualVinte = 10 == 20;
         boolean isDezDiferenteDez = 10 != 10.0;
-        
+
         System.out.println("isDezMaiorQueVinte "+isDezMaiorQueVinte );
         System.out.println("isDezMenorQueVinte " +isDezMenorQueVinte);
         System.out.println("isDezIgualVinte " +isDezIgualVinte);
         System.out.println("isDezIgualDez" +isDezDiferenteDez);
+
+        // And(&&) - Or (||)
+        int idade = 29;
+        float salario = 3500f;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta" +isDentroDaLeiMaiorQueTrinta );
+        System.out.println("isDentroDaLeiMenorQueTrinta" +isDentroDaLeiMenorQueTrinta);
 
 
     }
