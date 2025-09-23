c8ad5d9dfcc9ce51854f29065c26085bd7405c9a
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-09-22 22:38:41 -0300

    operadores atribuição

--- maratona-devdojo/exercicios/src/aula1/Aula04Operadores.java
+++ maratona-devdojo/exercicios/src/aula1/Aula04Operadores.java
@@ -47,6 +47,26 @@ public class Aula04Operadores {
 
         System.out.println("isPlaStationCincoCompravel" +isPlayStationCincoCompravel);
 
+        // = += -= *= /= %=
+        double bonus = 1800;
+        bonus += 1000;
+        bonus -= 1000;
+        bonus *= 2;
+        bonus /= 2;
+        bonus %= 2;
+
+        System.out.println(bonus);
+
+        //
+
+        int contador = 0;
+        contador += 1;
+        contador++;
+        contador--;
+        int contador2 = 0;
+        System.out.println(contador2++);
+        System.out.println(contador);
+
 
     }
 
