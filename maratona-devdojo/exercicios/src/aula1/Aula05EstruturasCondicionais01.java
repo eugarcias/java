93b4aab1d5544b3a1aaaed72b39dc83cc1ce8223
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-09-23 21:27:44 -0300

    att

--- maratona-devdojo/exercicios/src/aula1/Aula05EstruturasCondicionais01.java
+++ maratona-devdojo/exercicios/src/aula1/Aula05EstruturasCondicionais01.java
@@ -1,20 +1,22 @@
 package maratona.devdojo;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
 
-        int idade = 20;
+        int idade = 15;
         boolean isAutorizadoComprarBebida = idade >= 18;
 
         // !
-        if (isAutorizadoComprarBebida) {
+        if (isAutorizadoComprarBebida != false) {
             System.out.println("Autorizado a compra bebida alcólica");
 
-        }
-        if (isAutorizadoComprarBebida ==  false){
+        } else {
             System.out.println("Não autorizado a comprar bebida alcólica");
         }
-        if (idade){
+        if (isAutorizadoComprarBebida){
+            System.out.println("Não autorizado a comprar bebida alcólica");
+        }
+
             System.out.println("Fora do If");
 
         }
