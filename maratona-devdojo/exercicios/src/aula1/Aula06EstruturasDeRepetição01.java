1c762db6d8aea21d3e39a657cf3260c9008a7f01
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-09-26 07:27:42 -0300

    while, do-while e for

---
+++ maratona-devdojo/exercicios/src/aula1/Aula06EstruturasDeRepetição01.java
@@ -0,0 +1,20 @@
+package maratona.devdojo;
+
+public class Aula06EstruturasDeRepetição01 {
+    public static void main(String[] args) {
+        // while, do while, for
+        int count = 0;
+        while (count < 10) {
+            System.out.println(++count);
+        }
+        do {
+            System.out.println("dentro do do-while");
+        } while (count < 10);
+
+
+        for (int i=0; i < 10; i++){
+            System.out.println("For " +i);
+
+        }
+    }
+}
