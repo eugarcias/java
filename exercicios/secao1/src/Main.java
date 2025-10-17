3bd4d9411d0b7d4429b7776501931d3c93ad0729
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-10-16 22:36:01 -0300

    add

---
+++ exercicios/secao1/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // tipo - valor - atribuir o valor da variavel
+
+        String nome = "Mauricio";
+        int idade = 21;
+
+        System.out.println(nome + idade);
+
+
+    }
+}
