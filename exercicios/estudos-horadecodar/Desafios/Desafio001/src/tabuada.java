064820688d243619b290be096799a2779c6b720d
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-10-19 13:02:32 -0300

    att

--- exercicios/estudos-horadecodar/Desafios/Desafio001/src/tabuada.java
+++ exercicios/estudos-horadecodar/Desafios/Desafio001/src/tabuada.java
@@ -1,21 +1,52 @@
 import java.util.Scanner;
 
 public class tabuada {
-    private static Object scanner;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("--Tabuada---\n" +
-                "Insira um número: ");
+        System.out.println("Escolha um número: ");
 
         int num1 = scanner.nextInt();
+
+        System.out.println("--Tabuada---\n" +
+                "1 - Adição\n" +
+                "2 - Subtração\n" +
+                "3 - Multiplicação\n" +
+                "4 - Divisão");
+
+        int operacao = scanner.nextInt();
 
         System.out.println("Insira outro número: ");
         int num2 = scanner.nextInt();
 
         System.out.println();
 
-        System.out.println(num1 + num2);
+        switch (operacao) {
+            case 1:
+                System.out.println(num1 + "+" + num2 + "=" + (num1 + num2));
+                break;
+
+            case 2:
+                System.out.println(num1 + "-" + num2 + "=" + (num1 - num2));
+                break;
+
+            case 3:
+                System.out.println(num1 + "*" + num2 + "=" + (num1 * num2));
+                break;
+
+            case 4:
+                if (num2 == 0) {
+                    System.out.println("Erro: não é possivel dividir por zero");
+                } else {
+                    System.out.println(num1 + "/" + num2 + "=" + ((double) num1 / num2));
+                }
+                break;
+
+            default:
+                System.out.println("Operação Inválida! Por favor, insira um número");
+                break;
+        }
+        scanner.close();
     }
 }
