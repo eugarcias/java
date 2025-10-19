1d9718268f62e74fdf0a73424773ebcf7b9ab204
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-10-18 21:10:20 -0300

    att

---
+++ exercicios/secao1/Variaveis/src/ex005.java
@@ -0,0 +1,35 @@
+public class ex005 {
+    public static void main(String[] args) {
+        {
+
+
+            int primeiroValor = 10;
+            int segundoValor = 20;
+            int soma = primeiroValor + segundoValor;
+
+            System.out.println(soma);
+
+            char letra = 'A';
+
+            System.out.println((int) letra);
+
+            double priValor = 10.75;
+            double segValor = 20.75;
+
+            double soma2 = (priValor + segValor);
+
+            System.out.println(soma2);
+
+            long valorBilion = 2_000_000_000L;
+
+            int valorInteiro = (int )valorBilion;
+            System.out.println (valorInteiro);
+
+            String apresentacao = "Olá, Mundo!";
+            String msg = apresentacao +  "Bem-Vindo ao Java";
+            System.out.println(msg);
+
+        }
+        }
+    }
+
