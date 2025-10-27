f951e7b69126d7388186c8b43d93b1b7b42e6146
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-10-27 20:38:59 -0300

    att

--- exercicios/estudos-horadecodar/Condicionais/src/Main.java
+++ exercicios/estudos-horadecodar/Condicionais/src/Main.java
@@ -3,5 +3,23 @@
 public class Main {
     public static void main(String[] args) {
 
+        System.out.printf("Hello and welcome!");
+
+        // 1 - Boolean é true ou false
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        System.out.println(isFalse);
+
+        // - Operadores de Comparação
+
+        int x = 10;
+
+        System.out.println(x == 10);
+        System.out.println(x != 9);
+        System.out.println(x != 5);
+        System.out.println(x < 10);
+        System.out.println(x > 5);
+
     }
 }
