a8463f4b85101dc65787a81868798c633075ab68
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-10-27 20:31:58 -0300

    att

---
+++ exercicios/estudos-horadecodar/Condicionais/src/Condicionais.java
@@ -0,0 +1,13 @@
+
+
+public class Condicionais {
+
+    public static void main(String[] args) {
+        // 1 -  O que é Boolean (true ou false
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        System.out.println(isFalse);
+    }
+}
