18da9df8de231c9c407fb6c215e566259b87f121
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-10-28 22:06:58 -0300

    att

--- exercicios/estudos-horadecodar/Condicionais/src/OperadoresLogicos.java
+++ exercicios/estudos-horadecodar/Condicionais/src/OperadoresLogicos.java
@@ -14,6 +14,30 @@ public class OperadoresLogicos {
 
         }
 
+        // 9 - OR - Se uma condição for falsa, será true
+
+        boolean estarChovendo = false;
+        boolean temGuardaChuva = true;
+
+        System.out.println(estarChovendo || temGuardaChuva);
+
+        System.out.println(10 > 20 && 100 == 200);
+
+        int idade2 = 15;
+        boolean ehMembro = true;
+
+        if (idade2 > 18 || ehMembro){
+            System.out.println("Você pode entrar!");
+        } else {
+            System.out.println("Não pode entrar!");
+        }
+
+        // 10 - Operador Not
+
+        System.out.println(estarChovendo);
+        System.out.println(!estarChovendo);
+        System.out.println(estarChovendo || !temGuardaChuva);
+
 
     }
 }
