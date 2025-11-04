b117356b442dbd56dc2e4f51b39883d39c19bc83
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2025-11-03 22:23:30 -0300

    interface

---
+++ exercicios/exercicio034/src/exercicio034/Exercicio034.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercicio034;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Exercicio034 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
