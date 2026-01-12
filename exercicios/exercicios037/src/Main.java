a722dcbb31f9a960868bbd11963cbd671018d3a7
Maurício Garcia <contato.mauriciogarcia@gmail.com>
2026-01-12 20:51:53 -0300

    funções

---
+++ exercicios/exercicios037/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // 1 - Criando a primeira função
+
+        saudacao();
+    }
+
+    public static void saudacao (){
+
+        System.out.println("Olá, está é minha primeira função");
+
+    }
+}
